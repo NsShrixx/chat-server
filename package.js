2565155f9028b2c494c289ac70a63b2f54be425c
NsShrixx <jayjaylaptop1102@outlook.com>
2025-03-20 11:35:01 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 {
-  "name": "chat-server",
+  "name": "chat-app",
   "version": "1.0.0",
+  "description": "A simple chat app using Node.js and Socket.io",
   "main": "server.js",
   "dependencies": {
     "express": "^4.18.2",
-    "socket.io": "^4.5.4",
-    "cors": "^2.8.5"
+    "socket.io": "^4.5.4"
   },
   "scripts": {
     "start": "node server.js"
+  },
+  "engines": {
+    "node": "16.x"
   }
 }
